1a9eea3ad62c4bb062b4aa38ae354bf0cb359e66
Cryptic Sentinel <140169658+CrypticSentinel@users.noreply.github.com>
2026-05-02 17:11:28 +0200

    section doppia

--- src/pages/CharacterDetail.tsx
+++ src/pages/CharacterDetail.tsx
@@ -1070,6 +1070,7 @@ const CharacterDetail = () => {
                         </div>
                       </div>
                     </section>
+                    </section>
                   </>
                 )}
 
